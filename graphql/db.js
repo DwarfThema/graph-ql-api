9279c089d08946f9de63185461d460466b3650e7
DwarfThema <86091261+DwarfThema@users.noreply.github.com>
2021-12-27 11:44:53 +0900

    use REST_API

--- graphql/db.js
+++ graphql/db.js
@@ -1,55 +1,15 @@
-let movies = [
-  { id: 1, name: "SpiderMan", score: 4 },
-  {
-    id: 2,
-    name: "Akira",
-    score: 5,
-  },
-  {
-    id: 3,
-    name: "Evangellion",
-    score: 5,
-  },
-  {
-    id: 4,
-    name: "Starwars",
-    score: 2,
-  },
-  {
-    id: 5,
-    name: "Zootopia",
-    score: 3,
-  },
-  {
-    id: 6,
-    name: "Toystory",
-    score: 4,
-  },
-];
+import fetch from "node-fetch";
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
 
-export const getMovies = () => movies;
-
-export const getById = (id) => {
-  const filteredMovies = movies.filter((movie) => movie.id === id);
-  return filteredMovies[0];
-};
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
+export const getMovies = (limit, rating) => {
+  let REQUEST_RUL = API_URL;
+  if (limit > 0) {
+    REQUEST_RUL += `limit=${limit}`;
   }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
+  if (rating > 0) {
+    REQUEST_RUL += `&minimum_rating=${rating}`;
+  }
+  return fetch(`${REQUEST_RUL}`)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
 };
